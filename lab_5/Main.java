68c05a045a3c8c9ee04e2bd05b908a7d75f36d48
Re1NForceD <66302516+Re1NForceD@users.noreply.github.com>
2020-06-10 19:42:15 +0300

    naming

--- lab_5/Main.java
+++ lab_5/Main.java
@@ -5,21 +5,21 @@ public class Main {
         char firstChar = 'a';
         char lastChar = 'a';
         Text text = new Text("At ataaata matar atata. Mara atata atat tatr ata at. Tamar ata ratar atataa.");
-        for (Sentence sen: text.getSentences()) {
-            int ind = -1;
-            for (int w=0; w<sen.getWords().length; w++) {
-                Letter[] charsOfWord = sen.getWords()[w].getLetters();
+        for (Sentence sentence: text.getSentences()) {
+            int index = -1;
+            for (int i=0; i<sentence.getWords().length; i++) {
+                Letter[] charsOfWord = sentence.getWords()[i].getLetters();
                 if (Character.toLowerCase(charsOfWord[0].getaChar())==firstChar && charsOfWord[charsOfWord.length-1].getaChar()==lastChar) {
-                    if (ind==-1) {
-                        ind = w;
+                    if (index==-1) {
+                        index = i;
                     }
-                    else if (sen.getWords()[w].getLetters().length>sen.getWords()[ind].getLetters().length) {
-                        ind = w;
+                    else if (sentence.getWords()[i].getLetters().length>sentence.getWords()[index].getLetters().length) {
+                        index = i;
                     }
                 }
             }
-            if (ind!=-1){
-                sen.delWord(ind);
+            if (index!=-1){
+                sentence.delWord(index);
             }
         }
         System.out.println(text);
